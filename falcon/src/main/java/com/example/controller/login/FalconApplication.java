9dfb6d13817b50c0e267d571ecc123e968a67e2a
村井俊介 <@SAPPORO-03>
2020-03-02 16:46:00 +0900

    new

---
+++ falcon/src/main/java/com/example/controller/login/FalconApplication.java
@@ -0,0 +1,13 @@
+package com.example.controller.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalconApplication.class, args);
+	}
+
+}
